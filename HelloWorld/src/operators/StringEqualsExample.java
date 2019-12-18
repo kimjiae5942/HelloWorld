5905ff33e313cd16d9d8304885f3a6d0339a8336
User <User@YD01-10>
2019-12-18 12:49:39 +0900

    commit

--- HelloWorld/src/operators/StringEqualsExample.java
+++ HelloWorld/src/operators/StringEqualsExample.java
@@ -2,16 +2,16 @@ package operators;
 
 public class StringEqualsExample {
   public static void main(String[] args) {
-//	String str1 = "신민철";
-//	String str2 = "신민철";
-//	String str3 = new String("신민철");
-//	
-//	System.out.println(str1==str2);
-//	System.out.println(str2==str3);
-//	
-//	System.out.println(str1.equals(str2));
-//	System.out.println(str1.equals(str3));
-//	
+	String str1 = "신민철";
+	String str2 = "신민철";
+	String str3 = new String("신민철");
+	
+	System.out.println(str1==str2);
+	System.out.println(str2==str3);
+	
+	System.out.println(str1.equals(str2));
+	System.out.println(str1.equals(str3));
+	
 	int var1 = 10;
 	int var2 = 20;
 	int var3 = 30;
